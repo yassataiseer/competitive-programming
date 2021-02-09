56791be422b39442f883cd7049e15dad9d3e3801
Yassa Taiseer <yassataiseer@gmail.com>
2021-02-09 09:56:21 -0500

    commit on master

---
+++ j4-2019-UW.py
@@ -0,0 +1,18 @@
+def flipper(query):
+    line1 = [1,2]
+    line2 = [3,4]
+    for i in query:
+        if i =="H":
+            line1,line2 = line2,line1
+        else:
+            line1 = line1[::-1]
+            line2 = line2[::-1]
+    print (str(line1[0]) + " " + str(line1[1]))
+    print (str(line2[0]) + " " + str(line2[1]))
+            
+
+
+
+
+query = input()
+flipper(query)
