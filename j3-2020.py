f1b4e84cdbb8b416a0e9d3580769faf4316a128e
Yassa Taiseer <yassataiseer@gmail.com>
2021-02-09 12:41:38 -0500

    commit on master

---
+++ j3-2020.py
@@ -0,0 +1,14 @@
+def solve(top_right,bottom_left):
+    print(str(min(top_right)-1)+", "+str(min(bottom_left)-1))
+    print(str(max(top_right)+1)+", "+str(max(bottom_left)+1))
+
+
+top_right = []
+bottom_left = []
+coordinate_amount = int(input())
+for i in range(coordinate_amount):
+    coordinate = input()
+    first,secound = coordinate.split(",")
+    top_right.append(int(first))
+    bottom_left.append(int(secound))
+solve(top_right,bottom_left)
