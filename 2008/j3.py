0cdf5f09bcd779f8b3e465e550a9a0b5527331f0
Yassa Taiseer <yassataiseer@gmail.com>
2021-03-07 22:42:57 -0500

    commit on master

---
+++ 2008/j3.py
@@ -0,0 +1,29 @@
+def solve(Name,Number,trail):
+    name = ""
+    counter = 0
+    for i in range(len(Number)):
+        counter1 = 0
+        for j in range(len(trail)):
+            if Number[i] == trail[j]:
+                counter1 +=1
+                        
+            if counter < counter1 :
+                counter = counter1
+                name = Name[i]
+    return name                
+
+
+book = int(input())
+Name = []
+Number = []
+trail = []
+for i in range(book):
+    x = input()
+    x = x.split(" ")
+    Name.append(x[0])
+    Number.append(int(x[1]))
+calls = int(input())
+for i in range(calls):
+    number = int(input())
+    trail.append(number)
+print(solve(Name,Number,trail))
---
+++ 2018-CCC/j3-2018.py
@@ -0,0 +1,15 @@
+def solve(data):
+    counter = 0
+    prev = 0
+    for i in range(len(data)):
+        print(int(data[i])+int(data[counter])+prev)
+        prev += int(data[i])+int(data[counter])
+        counter+= 0
+def reformat( data):
+    data = data.split
+        #prev +=abs(int(data[count])-int(data[i]))
+
+#measure = input()
+#measure = measure.split(" ")
+#measure.insert(0,"0")
+reformat("3,0,10,12,5")
