ac071953cb128add6211d0535e573981f9b58747
Yassa Taiseer <yassataiseer@gmail.com>
2021-03-11 00:11:59 -0500

    commit on main

--- 2016-CCC/j3-2016.py
+++ 2016-CCC/j3-2016.py
@@ -1,9 +1,42 @@
-def solve(word):
-    for i in word
-    
+def front_solve(word):
+    counter = 0
+    new_word = word
+    while len(word)!=counter:
+        if new_word[::-1]==new_word:
+            return len(new_word)
+        elif len(new_word)==1:
+            return 1    
+        else:
+            new_word = new_word[0:len(new_word)-1]
+        counter+=1
+    return len(new_word)
+def back_solve(word):
+    counter = 0
+    new_word = word
+    while len(word)!=counter:
+        if new_word[::-1]==new_word:
+            return len(new_word)
+        elif len(new_word)==1:
+            return 1    
+        else:
+            new_word = new_word[1:len(new_word)]
+        counter+=1
+    return len(new_word)
+
+def both_solve(word):
+    counter = 0
+    new_word = word
+    while len(word)!=counter:
+        if new_word[::-1]==new_word:
+            return len(new_word)
+        elif len(new_word)==1:
+            return 1    
+        else:
+            new_word = new_word[1:len(new_word)]
+            new_word = new_word[0:len(new_word)-1]
+        counter+=1
+    return len(new_word)
 
 
-
-a = input()
-if len(a)==1:
-    return "1"
+word = input()
+print(max(front_solve(word),back_solve(word),both_solve(word)))
