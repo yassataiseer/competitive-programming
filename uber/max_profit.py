7557ec9341a7fc314e4e1091d0657867ceeb9fea
Yassa Taiseer <yassataiseer@gmail.com>
2021-03-25 01:58:16 -0400

    logN solution

--- uber/max_profit.py
+++ uber/max_profit.py
@@ -1,8 +1,9 @@
-def find_best_sum(track):
-    profits = 0
-    for i in range(len(track)):
-        for j in range(len(track)):
-            if track[j]-track[i]>profits:
-                profits = track[j]-track[i]
-    return profits
-print(find_best_sum([10, 7, 5, 8, 11, 9]))
+
+
+def find_best_sum_2(track):
+    best_high = max(track)
+    new_market = track[0:track.index(best_high)]
+    if track.index(best_high)==0:
+        return -1
+    return best_high - min(new_market)
+print(find_best_sum_2([10, 9, 7, 6, 4]))
