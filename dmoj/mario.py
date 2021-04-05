3d7307a6a3684130cb6b242baf93a4f565fe701e
Yassa Taiseer <yassataiseer@gmail.com>
2021-04-05 02:38:08 -0400

    commit on mater

--- dmoj/mario.py
+++ dmoj/mario.py
@@ -1,16 +1,45 @@
 def solve(data):
     answers = []
-    for level in data:
-        level = level.split("-")
-        if level[0]>4:
-            value = simple_solve(data)
-            answer.append(value)
-
+    level = data.split("-")
+    if int(level[0])>4:
+        value = simple_solve(level)
+        answers.append(value)
+    elif int(level[0])==4 and int(level[1])>2:
+        value = rough_solve(level)
+        answers.append(value)
+    else:
+        value = rough_solve(level)
+        answers.append(value)
     return answers
 def simple_solve(data):
     answer = 0
     answer += 4 - int(data[-1]) +1
     answer += (8 - int(data[0]))*4
     return answer
+def rough_solve(data):
+    answer = 0
+    while True:
+        if int(data[0])==1 and int(data[1])==2:
+            answer+=8
+            break
+        elif int(data[0])==4 and int(data[1])==2:
+            answer+=5
+            break
+        else:
+            data[1] = str(int(data[1])+1)
+            if int(data[1])>4:
+                data[1] = "1"
+                data[0] = str(int(data[0])+1)
+    return answer
 
-#print(simple_solve(["6","1"]))
+#print(solve("6-1"))
+#x  ="6-1"
+#x = x.split("-")
+#print(int(x[0]))
+data = []
+amount = int(input())
+for i in range(amount):
+    x = input()
+    data.append(x)
+for stuff in data:
+    print(solve(stuff)[0])
