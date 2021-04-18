db604d048ff58e24ead840dd7b4a18da9a958435
Yassa Taiseer <yassataiseer@gmail.com>
2021-04-18 00:29:21 -0400

    solved

--- 2021-CCC/j4-2021.py
+++ 2021-CCC/j4-2021.py
@@ -1,35 +1,22 @@
-
-
-
 def solve(query):
-    values = {"L":3,"M":2,"S":1}
     answer = 0
-
-    query = list(query)    
-    prev_letter = query[0]
-    counter = 0
-    for letter in query:
-        if values[letter]<values[prev_letter]:
-            prev_letter =  letter
-            counter+=1
-        elif values[letter]>values[prev_letter]:
-            location = query.index(prev_letter)
-            current_loc = query.index(letter)
-            query[location],query[counter] = query[counter],query[location]
+    table = {"L":3,"M":2,"S":1}
+    data = []
+    for letters in query:
+        data.append(table[letters])
+    sorted_data = sorted(data)
+    sorted_data = sorted_data[::-1] 
+    for i in range(len(data)):
+        if sorted_data[i]!=data[i]:
             answer+=1
-            counter+=1
-        else:
-            counter+=1
-            pass
-    return answer
+    return answer-1
+x = input()
+answer = solve(x)
 
-query = input()
-print(solve(query))
-
-
-
-
-
+if answer<0:
+    print(0)
+else:
+    print(answer)
 
 
 
