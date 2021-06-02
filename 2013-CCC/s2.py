7ec6883604a8c899af42055b3bad67d506f79b2d
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-02 18:54:15 -0400

    commit

--- 2013-CCC/s2.py
+++ 2013-CCC/s2.py
@@ -3,16 +3,16 @@ def solve(max_weight,car_weights):
     current_weight = 0
     counter = 0
     for i in range(len(car_weights)):
-        current_weight += car_weights[i]
-        if current_weight>max_weight:
-            break
+        if i ==0:
+            if sum(car_weights[i:i+4])>max_weight:
+                break
+            else:
+                answer+=4
         else:
-            answer+=1
-        counter+=1
-        if counter==4:
-            current_weight = 0
-            counter = 0
-            #answer-=1
+            if sum(car_weights[i:i+4])>max_weight:
+                break
+            else:
+                answer+=1            
     return answer
 
 ##print(solve(100,[50,30,10,10,40,50]))
