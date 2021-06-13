ba292e4465e127819bc19629cf10e5690a671832
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-13 17:33:15 -0400

    8/15

---
+++ 2009-CCC/s1.py
@@ -0,0 +1,16 @@
+def solve(min_value,max_value):
+    final_answer = 0
+    answers = []
+    while min_value<max_value:
+    
+        if min_value*min_value in answers and min_value*min_value<max_value:
+            final_answer+=1
+        answers.append(min_value*min_value)
+        if min_value*min_value*min_value in answers and min_value*min_value*min_value<max_value:
+            final_answer+=1
+        answers.append(min_value*min_value*min_value )
+        min_value+=1
+    return final_answer 
+min_value = int(input())
+max_value = int(input())
+print(solve(min_value,max_value))
