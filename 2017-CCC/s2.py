5225968f303da66ce62c04d753279057c54ca975
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-22 01:07:57 -0400

    o(logN) tine

--- 2017-CCC/s2.py
+++ 2017-CCC/s2.py
@@ -2,27 +2,12 @@ def solve(data):
     counter = len(data)
     big = []
     small = []
-    while counter>0:
-        try:
-            counter-=1
-            if len(data)==1:
-                if data[0]>max(small):
-                    big.append(data[0])
-                elif data[0]<max(small):
-                    small.append(data[0])
-            big.append(max(data))
-            data.remove(max(data))
-            small.append(min(data))
-            data.remove(min(data))
-        except ValueError:
-            break
-    while min(big)<max(small):
-        big1 = max(big)
-        small1 = max(small)
-        big.remove(big1)
-        small.remove(small1)
-        big.append(small1)
-        small.append(big1)
+    if len(data)==1:
+        return data
+    data=sorted(data)
+    big = data[0:int(len(data)/2)]
+    small = data[int(len(data)/2):len(data)]
+
     small=sorted(small)
     big = sorted(big)
     small = small[::-1]
@@ -30,6 +15,7 @@ def solve(data):
     for i in range(max(len(big),len(small))):
         final.append(str(small[i]))
         final.append(str(big[i]))
+    final = final[::-1]
     print(" ".join(final))
 
 
