16f71003066d39081dd12d96420e394ffc40198a
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-27 03:22:07 -0400

    15/15

--- 2021-CCC/s1-2021.cpp
+++ 2021-CCC/s1-2021.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
-float solve(int fence_length[],int fence_width[],int fence){
-    float total = 0;
+string solve(int fence_length[],int fence_width[],int fence){
+    double total = 0;
     for (int i=0;i<fence;i++){
-        float length = fence_length[i]+fence_length[i+1];
+        double length = fence_length[i]+fence_length[i+1];
         length = length/2;
         total += length*fence_width[i];
     }
-    return total;
+    string str = to_string(total);
+    return str;
 }
 
 
