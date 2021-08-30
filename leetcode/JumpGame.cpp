4da4b5425d395c2880b9189580ecc8a8e7236675
Yassa Taiseer <yassataiseer@gmail.com>
2021-08-29 22:32:06 -0400

    commit

--- leetcode/JumpGame.cpp
+++ leetcode/JumpGame.cpp
@@ -16,25 +16,21 @@ public:
     int jump(vector<int>& nums) {
         int length = nums.size()-1;
         int steps = 0;
+        int current_length = 0;
         for(int i=0;i<length;i++){
-            if(nums[i]-length==0){
-                steps++;
+            if(current_length>=length){
                 return steps;
             }
-            if(i+1!=length){
-                int current_i = i;
-                if(i==0){
-                    current_i=1;
-                }
-                if(nums[i+1]+current_i==length){
-                    steps+=2;
-                    return steps;
-                }else{
+            else{
+                if(nums[i]==0){}
+                else{
+                    current_length += nums[i];
                     steps++;
                 }
-            }
-        }
 
+                }
+
+        }
         return steps;
     }
 };
