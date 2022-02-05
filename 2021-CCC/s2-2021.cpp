f9cb5aaddad80d2f82b7e2f3c80dabcfc78f4676
Yassa Taiseer <yassataiseer@gmail.com>
2022-02-05 14:18:25 -0500

    commit

--- 2021-CCC/s2-2021.cpp
+++ 2021-CCC/s2-2021.cpp
@@ -10,67 +10,47 @@
 #include<iomanip>	
 using namespace std;
 
-int solve(string grid[],string direction[],int number[],int instructions,int length,int width){
-    int G_count = 0;
-    for(int i=0;i<instructions;i++){
-        if(direction[i]=="R"){
-            for(int x=0;x<width;x++){
-                if (grid[number[i]-1][x]=='B'){
-                    grid[number[i]-1][x] = 'G';
-                    //G_count++;
-                }
-                else{
-                    grid[number[i]-1][x] = 'B';
-                    //G_count--;
-                }
-            }
-        }
-        else{
-            for(int x=0;x<length;x++){
-                if (grid[x][number[i]-1]=='B'){
-                    grid[x][number[i]-1] = 'G';
-                    //G_count++;
-
-                }
-                else{
-                    grid[x][number[i]-1] = 'B';
-                    //G_count--;
-                }
-            }
-        }
-    }
-     for(int i=0;i<length;i++){
-         size_t n= count(grid[i].begin(), grid[i].end(), 'G');
-         G_count+=n;
-     }
-    return G_count;
-
-}
 
 
 int main(){
-    int length;
-    cin >> length;
-    int width;
-    cin >> width;
-    string grid[width];
-    string row="";
-    for(int i=0;i<length;i++){
-        row +="B";
-    }  
-    
-    int actions;
-    cin >> actions;
-    string direction[actions];
-    int number[actions];
-    for(int x=0;x<actions;x++){
-        cin >> direction[x] >>number[x];
-    }
+    int m,n;
+    cin>>m;
+    cin>>n;
+    int answer = 0;
+    vector<vector<char> > Grid(m, vector<char> (n, 'B'));
+    int strokes;
+    cin >> strokes;
+    for(int i=0;i<strokes;i++){
+        char action;
+        cin>>action;
+        int length;
+        cin>>length;
+        if(action=='R'){
+            for(int i=0;i<n;i++){
+                if(Grid[length-1][i]=='B'){
+                Grid[length-1][i]='G';
+                answer++; 
+                }else{
+                Grid[length-1][i]='B';
+                answer--;                
+                }
 
+            }
+        }
+        else if(action=='C'){
+            for(int i=0;i<m;i++){
+                if(Grid[i][length-1]=='B'){
+                Grid[i][length-1]='G';
+                answer++; 
+                }else{
+                Grid[i][length-1]='B';
+                answer--;                
+                }
 
-    for(int i=0;i<width;i++){
-        grid[i] = row;
+            }
+        }
+        
     }
-    cout << solve(grid,direction,number,actions,length,width)<<endl;
+    cout<<answer<<endl;
     return 0;
 }
